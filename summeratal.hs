b7b9741fbc7dbd61fd0ef3d55e1489fa87a7794e
KIMN <kimnor@kth.se>
2025-09-09 20:24:44 +0200

    experimenting with prints to get values of lenghts in print

--- summeratal.hs
+++ summeratal.hs
@@ -2,8 +2,11 @@ import Data.List (sort)
 
 main :: IO ()
 main = do
-    let ints = [1, 5, 8, 3,2]
+    let ints = [1, 5, 8, 3]
     putStrLn $ "Strings: " ++ show (sort ints)
     putStrLn $ "lenght: " ++ show (length ints)
-    putStrLn $ "Sortlenght: " ++ show (length $ sort ints)
+    putStrLn $ "lenght even: " ++ show (even $ length ints)
+    putStrLn $ "lenght if: " ++ show (div (length ints) 2)
+    putStrLn $ "lenght mod: " ++ show (mod (length ints) 2)
+    putStrLn $ "Sortlenght2: " ++ show (length $ sort ints)
     putStrLn $ "Sortlenght: " ++ show (length $ sort ints)
