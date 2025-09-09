155abcbfa921ad66dc78855042bb20a3a926ae6b
KIMN <kimnor@kth.se>
2025-09-09 20:53:56 +0200

    experimenting with print to get right sum

--- summeratal.hs
+++ summeratal.hs
@@ -2,11 +2,16 @@ import Data.List (sort)
 
 main :: IO ()
 main = do
-    let ints = [1, 5, 8, 3]
+    let ints = [1, 2, 7,8,9]
+    putStrLn $ "Strings: " ++ show (sort ints)
     putStrLn $ "Strings: " ++ show (sort ints)
     putStrLn $ "lenght: " ++ show (length ints)
     putStrLn $ "lenght even: " ++ show (even $ length ints)
-    putStrLn $ "lenght if: " ++ show (div (length ints) 2)
+    putStrLn $ "lenght div: " ++ show (div (length ints) 2)
     putStrLn $ "lenght mod: " ++ show (mod (length ints) 2)
-    putStrLn $ "Sortlenght2: " ++ show (length $ sort ints)
+    putStrLn $ "lenght div+mod: " ++ show (div (length ints) 2 + mod (length ints) 2)
+   
     putStrLn $ "Sortlenght: " ++ show (length $ sort ints)
+    putStrLn $ "drop s lenght div+mod from sorted ints: " ++ show(drop (div (length ints) 2) (sort( ints)))
+    putStrLn $ "take lenght div+mod from sorted ints: " ++ show(drop (div (length ints) 2 + mod (length ints) 2) (sort( ints)))
+    putStrLn $ "average of top: " ++ show (sum(drop (div (length ints) 2)  (sort( ints))))
