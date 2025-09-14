aefb9fc0e2de55912ebf96beef4823ef2aa0d362
kimnor <kimnor@kth.se>
2025-09-14 23:27:53 +0200

    Edited after deadline : implementing 'map read' and refactoring with $

---
+++ summeratalEditedAfterDeadline.hs
@@ -0,0 +1,63 @@
+import Data.List (sort)
+import Data.List (splitAt)
+    -- UNFORTUNATLY I DIDNT GET THE CODE TO WORK 100%
+    -- IT'S DOES CALCULATE THE SUM OF TOP NUMBERS BUT IT IS
+    -- UNREADABLE AND UGLY AND HARD TO MAINTAIN code. 
+    -- ILL  TRY TO REFACTOR IT LATER WITH PROPER SYNTAX.
+    -- string conversion to int needed
+
+--sumThehighNumbers :: [Int] -> int
+    --sum(drop (div (length ints) 2)  (sort( ints)))
+--convertToInt:: string -> int
+--convertToInt s = s :: Int
+main :: IO ()
+main = do
+    print "Welcome! press enter to  keep Kattis happy, no typing nececary (redundant input from kattis)"
+    _ <- getLine
+    print "Enter input in format 5 2 3 2 1 1 etc (conversion to list not handled yet)"
+    line2 <- getLine
+    let listOfIntAsStrings  =  words $ line2
+        lenghtOfList = length listOfIntAsStrings
+        x =  sum $ map (read :: String-> Int) $ drop (lenghtOfList `div` 2) $ sort $ listOfIntAsStrings
+        in  putStrLn $ show $ x
+    --putStrLn $ "mapexsum :" ++ show(sum . map (read :: String-> Int)  $ drop (div (length ) 2)  $ words $ line2)
+    --print show(take 1 . words  line)
+
+    print "string to int conversion through maps planned feature"
+       -- planned function here, map convert string to int on 'word line' out put to ints                                                                                     
+    let ints = [5,3,2,1,1] 
+        in putStrLn $ show ints ++ "The sum of top " ++ show (length ints - (div (length ints) 2)) ++ " ints: " ++
+        show (sum(drop(div (length ints) 2)  (sort( ints))))
+
+    {--putStrLn $ "Ints: " ++ show (ints)
+    --putStrLn input-
+    
+    --putStrLn input2
+    {--let wordList = words "1 14 67 83 42 6 17 33 91"
+        ints = map putstr wordList :: Int --function that makes string to Int , put it qfter map
+        
+    in putStrLn $ show(sum(drop (div (length ints) 2)  (sort(ints))))
+    --}
+    --putStrLn $ "Strings: " ++  show (words myString)
+    --let myListOfWords = words myString
+    --putStrLn $ "Strings: " ++  show (words myString :: [Int])
+    --let ints =  words myString --have to mconvert elements to int somehow
+    --map stringToInteger (words myString)
+    --print words myString
+   
+    putStrLn $ "sorted list: " ++ show (sort ints)
+ 
+    putStrLn $ "lenght: " ++ show (length ints)
+    putStrLn $ "lenght even: " ++ show (even $ length ints)
+    putStrLn $ "lenght div: " ++ show (div (length ints) 2)
+    putStrLn $ "lenght mod: " ++ show (mod (length ints) 2)
+    putStrLn $ "lenght div+mod: " ++ show (div (length ints) 2 + mod (length ints) 2)
+   
+    putStrLn $ "Sortlenght: " ++ show (length $ sort ints)
+    --putStrLn $ "drop s lenght div+mod from sorted ints: " ++ show(drop (div (length ints) 2) (sort( ints)))
+    putStrLn $ "take lenght div+mod from sorted ints: " ++ show(drop (div (length ints) 2 + mod (length ints) 2) (sort( ints)))
+    --
+    
+        --putStrLn $ show(myString)
+
+    --- let complicated x=2 , y=5 in --}
